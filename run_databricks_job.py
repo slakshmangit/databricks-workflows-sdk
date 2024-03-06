eeeecaa0cfb6c9d342606a5137afddb9a589d608
LakshmanS <lakshman.santhanam@gmail.com>
2024-03-06 08:19:30 +0530

    Update run_databricks_job.py

--- run_databricks_job.py
+++ run_databricks_job.py
@@ -1,20 +1,21 @@
 """ 
-This is a minimal POC to run databricks orchestration workflow jobs via YAML 
-configuration using Python SDK. This will be helpful for Data Science team to not rely on specific
-Orchestration tools like Airflow and make any code changes. Instead, change the config
-and run the workflow job. This can be enhanced further in CI/CD and automations.
+This script demonstrates a proof-of-concept (POC) for running Databricks orchestration workflow jobs via YAML 
+configuration using the Python SDK. It provides an alternative for Data Science teams to avoid dependency 
+on specific orchestration tools like Airflow, enabling them to make changes directly in the configuration 
+and execute the workflow job. This approach can be further integrated into CI/CD pipelines and automation processes.
 
-1. Imports necessary databricks sdk modules to run databricks workflow 
-2. Use yaml, json to parse the config
-3. Create workflow job in databricks as per the configuration input
-4. Triggers the job with given params in the config
-5. Deletes the job once completed (Commented code)
+Features:
+1. Imports necessary Databricks SDK modules to run Databricks workflow.
+2. Utilizes YAML and JSON to parse the configuration.
+3. Creates a workflow job in Databricks based on the provided configuration input.
+4. Triggers the job with given parameters in the config.
+5. Optionally deletes the job once completed (Commented code).
 
 Pre-requisites:
 %pip install --upgrade databricks-sdk
 
 Databricks Profile Setup at ~/.databrickscfg
-refer. https://docs.databricks.com/en/dev-tools/cli/profiles.html
+Refer: https://docs.databricks.com/en/dev-tools/cli/profiles.html
 
 """
 import time
@@ -23,40 +24,45 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service import jobs, compute
 import json
 
-DATABRICK_PROFILE="dev" # can be an env variable
+DATABRICK_PROFILE = "dev"  # Can be set as an environment variable
 
 w = WorkspaceClient(profile=DATABRICK_PROFILE)
 
-def config_parser(config_file: str):
+def config_parser(config_file: str) -> dict:
     """
-    Parses the YAML configuration file which has the details about the databicks job to be run
-    along with source code and run time params.
+    Parses the YAML configuration file containing details about the Databricks job to be executed,
+    including source code and runtime parameters.
 
-    Input: config_file - yaml configuration file path
-    Output: Dictionary - Key-value mappings of the Job and tasks
+    Args:
+    - config_file (str): Path to the YAML configuration file.
+
+    Returns:
+    - dict: Key-value mappings of the Job and tasks.
     """
     try:
         with open(config_file) as f:
             configuration = yaml.safe_load(f)
-            output = {}
-            output["job"] = {}
+            output = {"job": {}}
             for k, v in configuration["job"].items():
                 output["job"][k] = configuration["job"][k]
             return output
     except Exception as ex:
         raise ValueError(
-            "Failied to initialize ApplicationConfiguration, couldn't load yaml config!"
+            "Failed to initialize ApplicationConfiguration, couldn't load YAML config!"
         ) from ex
 
 
-def create_tasks(tasks_list_input: list):
+def create_tasks(tasks_list_input: list) -> list:
     """
-    Dynamically creates tasks objects along with the provided configurations.
+    Dynamically creates task objects based on the provided configurations.
 
-    Input: tasks_list_input - list with tasks and details
-    Output: a list of Databricks task objects
+    Args:
+    - tasks_list_input (list): List containing task details.
+
+    Returns:
+    - list: A list of Databricks task objects.
     """
-    tasks_list_output: list = []
+    tasks_list_output = []
     for tasks_dict in tasks_list_input:
         task = jobs.Task(
             description=tasks_dict["description"],
@@ -69,10 +75,8 @@ def create_tasks(tasks_list_input: list):
             task_key=tasks_dict["task_key"],
             timeout_seconds=0,
             depends_on=[
-                jobs.TaskDependency(task_key=i) for i in tasks_dict["depends_on"]
-            ]
-            if "depends_on" in tasks_dict
-            else None,
+                jobs.TaskDependency(task_key=i) for i in tasks_dict.get("depends_on", [])
+            ],
         )
         tasks_list_output.append(task)
     return tasks_list_output
@@ -82,14 +86,11 @@ config = config_parser("src/configuration.yaml")
 job = config["job"]
 tasks = job["tasks"]
 
-
-task_list = []
-for task in tasks:
-    task_list.append(task["task_key"])
-
+task_list = [task["task_key"] for task in tasks]
 task_names = ",".join(task_list)
 
 job_tasks = create_tasks(tasks)
+
 # Create the Job with tasks and configuration from config_file
 created_job = w.jobs.create(
     name=f"{job['name']}-{time.time_ns()}",
@@ -115,5 +116,5 @@ created_job = w.jobs.create(
 # Run the Created Job
 run_by_id = w.jobs.run_now(job_id=created_job.job_id).result()
 
-# cleanup - uncomment to delete the job once complete.
+# Cleanup - Uncomment to delete the job once complete.
 # w.jobs.delete(job_id=created_job.job_id)
